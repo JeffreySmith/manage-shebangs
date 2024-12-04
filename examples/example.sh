7566c49d1104cefb6b32c3d6a0078bd7de7b5c03
Jeffrey Smith <jeffrey.smith@acceldata.io>
2024-12-04 11:00:16 -0500

    Simplified the example a little

--- examples/example.sh
+++ examples/example.sh
@@ -6,16 +6,18 @@
 # #!/usr/bin/env my_python_wrapper, unless that shebang includes 'impala-python'.
 find testdata/python -type f -name "*.py"\
      -exec ../manage_shebang.sh -f {} -e my_python_wrapper -v -i impala-python \;
-echo
-printf "Showing the first line of each python file\n"
+
+printf "\nShowing the first line of each python file\n"
+
 for f in testdata/python/*.py; do
     printf "%s: %s\n" "$f" "$(head -n 1 "$f")"
 done
+
 #Now that we've modified everything, set it back to what it was originally
 #using the .orig backup files we created
 for f in testdata/python/*.orig; do
     if [ -f "$f" ]; then
-        NAME="$(echo "$f" | awk -F"." '{for(i=1; i < NF; i++) output=output $i (i<NF-1 ? "." : "");} END{print output}')"
+       NAME="$(echo "$f"| sed 's/[.]orig$//')"
         \mv "$f" "$NAME"
     fi
 done
@@ -24,14 +26,16 @@ done
 #ksh (korn shell) instead of their current shell
 find testdata/shell -type f -name "*.sh"\
      -exec ../manage_shebang.sh -f {} -p /bin/ -e ksh -v \;
-echo
-echo "Printing new shell shebangs..."
+
+printf "\nPrinting new shell shebangs..."
+
 for f in testdata/shell/*.sh; do
     printf "%s: %s\n" "$f" "$(head -n 1 "$f")"
 done
+
 for f in testdata/shell/*.orig; do
     if [ -f "$f" ]; then
-        NAME="$(echo "$f" | awk -F"." '{for(i=1; i < NF; i++) output=output $i (i<NF-1 ? "." : "");} END{print output}')"
+        NAME="$(echo "$f"| sed 's/[.]orig$//')"
         \mv "$f" "$NAME"
     fi
 done
